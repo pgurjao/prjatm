c3ac8f947e986f72afd9c2314f7fb943e3271e93
PJ <PJ@PJ>
2021-02-09 20:31:05 -0300

    Segundo commit com menu pronto

--- src/main/java/is/lell/prjatm/Atm.java
+++ src/main/java/is/lell/prjatm/Atm.java
@@ -19,31 +19,32 @@ public class Atm {
 		
 		int i = 0;
 		int tamanho = Dados.contas.length;
+		int indiceConta = 0;
 		
 		
 		do  {
 
 			if (Dados.contas[i].equalsIgnoreCase(numeroConta)) {
-				System.out.println("\n" + "Conta localizada");
+				System.out.println("\n" + "Conta localizada \n");
+				indiceConta = i;
 				localizado = true;
+				
 			}
-			System.out.println("i == " + i);
+			System.out.println("Passagem nº == " + i);
 			System.out.println("localizado == " + localizado);
-			i++;
-			
-			
+			i++;			
 
 			// for (int i = 0 ; i < Dados.contas.length ; i++ ) {
 			// for (String i : Dados.contas) {			
 			// System.out.println(Dados.contas[i]);
 			// System.out.println(i);
 
-		}
+		} while ( localizado != true || i != Dados.contas.length );
 		//while ( localizado != true );
-		while ( i != Dados.contas.length );
+		//while ( i != Dados.contas.length );
 		
 		if (localizado == false ) {
-			System.out.println("Conta nao loacalizada \n");
+			System.out.println("Conta nao loacalizada \n\n");
 		}
 
 		System.out.print("\n" 
@@ -62,10 +63,45 @@ public class Atm {
 
 		int choice = sc.nextInt();
 
-		if (choice < 4 ) {
-			System.out.println("\n" + "Opcao digitada: "  + choice);
-		}
+		switch (choice) {
+		case 1:
+			System.out.println("Verificando saldo..." + "\n");
+			System.out.println("Seu saldo é: " + Dados.saldos[indiceConta]);			
+			break;
+		
+		case 2:
+			System.out.println("Realizando saque..." + "\n");
+			System.out.println("Digite o valor a ser sacado: ");
+			double saque = sc.nextDouble();
+			
+			if(saque > Dados.saldos[indiceConta]) {
+				System.out.println("Saldo insuficiente \n");
+			} else {
+				Dados.saldos[indiceConta] = Dados.saldos[indiceConta] - saque; 
+			}
+			break;			
+		
+		case 3:
+			System.out.println("Depositando dinheiro..." + "\n");
+			System.out.println("Digite o valor a ser depositado: ");
+			double deposito = sc.nextDouble();
+			
+			if(deposito <= 0.00 ) {
+				System.out.println("O valor de deposito não pode ser menor ou igual a zero");
+			} else {			
+				Dados.saldos[indiceConta] = Dados.saldos[indiceConta] + deposito;
+			}
+			break;
+		
+		case 4:
+			System.out.println("Saindo...");
+			break;
 
+		default:
+			System.out.println("Opção inválida");
+			break;
+		}
+		
 		System.out.println("\n" + "F   i   m");
 		sc.close();
 	}
