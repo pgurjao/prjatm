1c44a0c9b9fad479b7785bb2c411c738049fd0ff
PJ <PJ@PJ>
2021-02-25 00:51:38 -0300

    MODIFY: criado arquivo modelo.enums.java com lista de string dos erros possíveis do sistema.

---
+++ src/main/java/is/lell/prjatm/modelo/enums.java
@@ -0,0 +1,15 @@
+package is.lell.prjatm.modelo;
+
+public class enums {
+	
+	public static enum statusEnum {
+		CONTA_LOCALIZADA,
+		CONTA_NAO_LOCALIZADA,
+		PIN_CORRETO_LOGIN_OK,
+		PIN_INCORRETO,
+		SALDO_INSUFICIENTE,
+		SAQUE_OK,
+		DEPOSITO_OK
+	}
+
+}
